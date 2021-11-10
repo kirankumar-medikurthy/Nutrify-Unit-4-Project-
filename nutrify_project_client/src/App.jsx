482378a5e5b173fc0cd1882d4fde57466ddb11d4
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-11-10 19:41:05 +0530

    App.jsx

--- nutrify_project_client/src/App.jsx
+++ nutrify_project_client/src/App.jsx
@@ -2,12 +2,18 @@
 import './App.css';
 import {Page} from "./components/Home/Page"
 import {Third} from "./components/Home/Third"
+import {Entrancepage} from "./components/Home/Entrancepage"
+import {SplashOne} from "./components/Splash_Screen/SplashOne"
+import {SplashTwo} from "./components/Splash_Screen/SplashTwo";
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Nutrify project</h1>
       <Page />
       <Third/>
+      <Entrancepage/>
+      <SplashOne/>
+      <SplashTwo/>
     </div>
   );
 }
