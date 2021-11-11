2bbc870524f08ce83da1b76d7b65b12a321572ff
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-11-12 01:20:28 +0530

    Created product detail page along with product price calculation page

--- nutrify_project_client/src/components/Home/Page.jsx
+++ nutrify_project_client/src/components/Home/Page.jsx
@@ -1,17 +1,29 @@
 import "./page.css";
-import Axios from 'axios';
 import {useEffect,useState} from "react";
+let data = [];
 function Page(){
-    const [data,setData] = useState([]);
     const [loading,setLoading] = useState(true);
     useEffect(()=>{
         getCategory();
       },)
-      const getCategory = async ()=>{
-          const {data} = await Axios.get("http://localhost:3001/categorys")
-          setData(data);
-          setLoading(false);
-      }
+    const getCategory = () => {
+        var xhr = new XMLHttpRequest();
+        var getUrl = "http://localhost:7878/categorys";
+        xhr.open("GET", getUrl);
+        xhr.send();
+        xhr.onload = function () {
+          var response = JSON.parse(this.response);
+          for (var i = 0; i < response.category.length; i++) {
+            if (i === 0) {
+              console.log(response.category[0].catergory_name);
+              data.push(response.category);
+              setLoading(false);
+            }
+            break;
+          }
+        };
+      };
+    
     return loading?"loading ....":(
         <>
             <div id="pageLayout">
@@ -31,18 +43,20 @@ function Page(){
                     <div id="inputbox">
                         <input id="inputcontent" type="text" placeholder="Type your problem area"/>
                     </div>
-                    <div id="text">You can select from below categories</div>
-                    <div id="icons">
-                            <div className="icon"><img className="img"src="/iconimages/Eye.png" alt={data[0].name}/><br/>{data[0].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/weight.png" alt={data[1].name}/><br/>{data[1].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/ear.png" alt={data[2].name}/><br/>{data[2].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/blood.png" alt={data[3].name}/><br/>{data[3].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/bones.png" alt={data[4].name}/><br/>{data[4].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/smile.png" alt={data[5].name}/><br/>{data[5].name}</div>
-                             <div className="icon"><img className="img"src="/iconimages/breathe.png" alt={data[6].name}/><br/>{data[6].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/joints.png" alt={data[7].name}/><br/>{data[7].name}</div>
-                            <div className="icon"><img className="img"src="/iconimages/energy.png" alt={data[8].name}/><br/>{data[8].name}</div>               
-                    </div>
+                    <div id="text">You can select from below categories</div>        
+                    {
+                        <div id="icons">
+                            <div className="icon"><img className="img"src="/iconimages/Eye.png" alt={data[0][0].catergory_name}/><br/>{data[0][0].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/weight.png" alt={data[0][1].catergory_name}/><br/>{data[0][1].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/ear.png" alt={data[0][2].catergory_name}/><br/>{data[0][2].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/blood.png" alt={data[0][3].catergory_name}/><br/>{data[0][3].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/bones.png" alt={data[0][4].catergory_name}/><br/>{data[0][4].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/smile.png" alt={data[0][5].catergory_name}/><br/>{data[0][5].catergory_name}</div>
+                             <div className="icon"><img className="img"src="/iconimages/breathe.png" alt={data[0][6].catergory_name}/><br/>{data[0][6].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/joints.png" alt={data[0][7].catergory_name}/><br/>{data[0][7].catergory_name}</div>
+                            <div className="icon"><img className="img"src="/iconimages/energy.png" alt={data[0][8].catergory_name}/><br/>{data[0][8].catergory_name}</div>           
+                        </div> 
+                    }
                 </div>
             </div>
         </>
---
+++ nutrify_project_client/src/components/Payment/NutriconSecond.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import "./Nutricon.css";
+let data = [];
+function NutriconSecond() {
+    const [count,setCount] = useState(1);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    handleNutricon();
+  },[count]);
+  const handleCount = (count) =>{
+      if(count !== 0) {
+        setCount(count);
+      }
+  }
+  const handleNutricon = () => {
+    var xhr = new XMLHttpRequest();
+    var getUrl = "http://localhost:7878/products";
+    xhr.open("GET", getUrl);
+    xhr.send();
+    xhr.onload = function () {
+      var response = JSON.parse(this.response);
+      for (var i = 0; i < response.product.length; i++) {
+        if (i === 0) {
+          //console.log(response.product[0].product_name);
+          data.push(response.product[0]);
+          setLoading(false);
+        }
+        break;
+      }
+    };
+  };
+  return loading ? (
+    "loading ..."
+  ) : (
+    <div id="NutriconSecondLayout">
+      <div id="Nutricionheading1"></div>
+      <div id="Nutriconheader">
+        <h3 id="Nutricionheading">Nutricon</h3>
+        <div id="Nutricontoggle-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className="Shopping-cart">Shopping Cart</div>
+      <div id="Nutricon-self-tracking">
+          <div className="Nutriocon-image-box">
+              <div>
+              <img src="/iconimages/Payments/nutricon.png" alt={data[0].product_name}/>
+              </div>
+              <div>
+                  <div className="Nutricon-self-tracking-data">Nutricon self nutrition tracking device</div>
+                  <div className="Nutricon-self-tracking-price">{`Rs ${data[0].Subtotal}`}</div>
+                  <div id="counterbtns">
+                        <button onClick={()=>{handleCount(count-1)}} id="minus">-</button>
+                        <button id="count">{count}</button>
+                        <button onClick={()=>{handleCount(count+1)}} id="plus">+</button>
+                  </div>
+              </div>
+          </div>
+          <div id="Order-summary">Order Summary</div>
+          <div id="orderSummaryBelowBox">
+              <div className="subtotal">Subtotal<span>{`Rs ${data[0].Subtotal}`}</span></div>
+              <div className="Discount">Discount<span>{`Rs ${data[0].Discount}`}</span></div>
+              <div className="Shipping">Shipping & Delivery charge<span>{data[0].Shipping_and_Delivery_charge}</span></div>
+              <div className="TotalAmount">Total amount<span><b>{`Rs ${count*data[0].Subtotal}`}</b></span></div>
+          </div>
+          <button id="proceed">Proceed</button>
+      </div>
+      {/* <h1>{data[0].product_name}</h1>
+      <h1>{data[0].Discount}</h1> */}
+    </div>
+  );
+}
+export { NutriconSecond };
