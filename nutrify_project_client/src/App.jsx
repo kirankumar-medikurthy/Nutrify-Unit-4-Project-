7cf4b444ccf5c1094467406925733d0410f74a1e
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-11-12 22:58:13 +0530

    Add all component in orderwise to make routing

--- nutrify_project_client/src/App.jsx
+++ nutrify_project_client/src/App.jsx
@@ -1,22 +1,27 @@
-
-import './App.css';
-import {Page} from "./components/Home/Page"
-import {Third} from "./components/Home/Third"
+import React from "react";
 import {Entrancepage} from "./components/Home/Entrancepage"
 import {SplashOne} from "./components/Splash_Screen/SplashOne"
 import {SplashTwo} from "./components/Splash_Screen/SplashTwo";
+//-------------------------Neha add you signup Functionality Here---------------------------->
+import {Homepage} from "./components/Home/Homeone";
+import {Page} from "./components/Home/Page"
+import {Third} from "./components/Home/Third"
+//<-----------------------Abhishek paste you code ----------------->
 import {Menu} from "./components/Signup/Menu";
+//-----------------------------Sumit paste your code here; --------------->
 import {Nutricon} from "./components/Payment/Nutricon";
 import {NutriconSecond} from "./components/Payment/NutriconSecond"
+import './App.css';
+// import {Parent} from "./components/Parent";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Nutrify project</h1>
-      <Page />
-      <Third/>
       <Entrancepage/>
       <SplashOne/>
       <SplashTwo/>
+      <Homepage/>
+      <Page/>
+      <Third/>
       <Menu/>
       <Nutricon/>
       <NutriconSecond/>
